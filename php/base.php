4d3c290c62582334a59b145ce79e9b9c45271579
sebaslds09 <ing.sebastianortiz@outlook.com>
2015-07-06 21:51:03 -0500

    First Steps
    Base view

---
+++ php/base.php
@@ -0,0 +1,29 @@
+<?PHP
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Sebastian C.V.</title>
+		<link rel="stylesheet" type="css/text" href="../css/base.css">
+		<link rel="stylesheet" type="css/text" href="../css/bootstrap.css">
+		<script type="javascript/text" href="../js/jquery-1.11.3.js"></script>
+		<script type="javascript/text" href="../js/base.js"></script>
+		<script type="javascript/text" href="../js/bootstrao.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+		<header class="container header-img">
+			<div class="row">
+				<div class="header-pic"><img src="../img/hv.jpg" class="hv-img"></div>
+			</div>
+		</header>
+		<div id="dBanner"></div>
+		<nav>
+			<ul id="uMenu">
+				<li>Home</li>
+			</ul>
+		</nav>
+		<main></main>
+	</body>
+</html>
