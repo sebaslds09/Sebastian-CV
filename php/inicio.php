2f893a3059b9073ed7f9c99947641fe286f51925
sebaslds09 <ing.sebastianortiz@outlook.com>
2015-08-11 06:16:12 -0500

    Build home
    some text here

--- php/inicio.php
+++ php/inicio.php
@@ -6,11 +6,21 @@
 <div class="container">
 	<div class="row">
 		<div class="notice col-md-5 col-xs-10 col-xs-offset-1">
-		
+			<div class="row">
+				<div class="col-md-6 col-xs-6">
+					<img class="img-responsive" src="./img/content/1.jpg">
+				</div>
+				<div class="col-md-6 col-xs-6 clearfix">
+					<p class="text-info">Soy una persona apasionada por la fotografia y por los retos mentales, me gusta ponerme a prueba resolviendo problemas y creando la escena. 
+					No me gusta forzar las cosas, siempre he pensado que las mejores ideas simplemente fluyen. Por eso me gusta despacio, analisando cada oportunidad que cada problema 
+					ofrece para ser solucionado, eso me ha dado la ventaja de poder apreciar cosas que la mayoría de gente no tiene tiempo para detenerse y analizar.</p>
+				</div>
+			</div>
 		</div>
 		<div class="notice col-md-5 col-xs-10 col-xs-offset-1 col-md-offset-1">
 		
 		</div>
-<div>
+	</div>
+</div>
 
 
