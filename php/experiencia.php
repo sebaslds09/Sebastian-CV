99b290d775caf82fcff5b35e571b2d0c92ad9db6
sebaslds09 <ing.sebastianortiz@outlook.com>
2015-08-25 06:03:27 -0500

    Experience
    A little about my experience

--- php/experiencia.php
+++ php/experiencia.php
@@ -10,11 +10,42 @@
 					<img class="img-responsive" src="./img/content/2.jpg">
 				</div>
 				<div  class="notice-text">
-					<p class="text-info" align="justify"></p>
+					<p class="text-info" align="justify"><b>Profesor Clubes Juveniles de Rob&oacute;tica</b><br><h5>Como profesor en el programa de Clubes Juveniles de Rob&oacute;tica de
+					la gobernación de Antioquia, recorrí dos municipios del Nordeste andtioqueño desarrolando un curso de rob&oacute;tica con jovenes de secundaria de diversas instituciones
+					educativas. Durante este proyecto tuve las responsabilidades de coordinar las sesiones, de manejar el grupo de estudiantes, de dictar el curso y capactiar el ensamblaje del rob&oacute;t</h5>
+					</p>
 				</div>
 			</div>
 		</div>
 		<div class="notice col-md-5 col-xs-10 col-md-offset-2">
-		
+			<div class="col-md-6 col-xs-6">
+					<img class="img-responsive" src="./img/content/3.jpg">
+				</div>
+				<div  class="notice-text">
+					<p class="text-info" align="justify"><b>Facilitador, Robotiando Ando</b><br><h5></h5>
+					</p>
+				</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="notice col-md-5 col-xs-10">
+			<div class="row">
+				<div class="col-md-6 col-xs-6">
+					<img class="img-responsive" src="./img/content/4.jpg">
+				</div>
+				<div  class="notice-text">
+					<p class="text-info" align="justify"><b></b><br><h5></h5>
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="notice col-md-5 col-xs-10 col-md-offset-2">
+			<div class="col-md-6 col-xs-6">
+					<img class="img-responsive" src="./img/content/5.jpg">
+				</div>
+				<div  class="notice-text">
+					<p class="text-info" align="justify"><b></b><br><h5></h5>
+					</p>
+				</div>
 		</div>
 	</div>
